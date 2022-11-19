4494b2ae7bdfdf23b7d8debd654eb9b4e1468a5d
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2022-11-19 12:32:59 +0800

    fixed bugs

--- Calculator-script.js
+++ Calculator-script.js
@@ -22,110 +22,69 @@ var textBox = document.getElementById("text").innerHTML;
 // Variables
 var currentNumber = "";
 var firstNumber = "";
-var secondNumber = "";
 var operation = "";
 var result = "";
 
 // Functions
 function inputNumber(inputNumber){
-    // 0 input
-    if (inputNumber == 0){
-        if (document.getElementById("text").innerHTML !== "0"){
-            currentNumber += inputNumber;
-            document.getElementById("text").innerHTML = currentNumber;
-        }
-    }
-    // Decimal input
-    else if (inputNumber == "."){
-        if (document.getElementById("text").innerHTML === "0"){
-            currentNumber += "0.";
-            document.getElementById("text").innerHTML = currentNumber;
-        } 
-        if (document.getElementById("text").innerHTML.includes(".") == false){
-            currentNumber += ".";
-            document.getElementById("text").innerHTML = currentNumber;
-        }
-    }
-    // Sign input
-    else if (inputNumber === "-"){
-        if (document.getElementById("text").innerHTML !== "0"){
-            if (document.getElementById("text").innerHTML.includes("-") == false){
-                currentNumber = "-" + currentNumber;
-                document.getElementById("text").innerHTML = currentNumber;
-            }
-            else {
-                currentNumber = currentNumber.slice(1)
-                document.getElementById("text").innerHTML = currentNumber;
-            }
-        } 
-    }
-    // Number Input
-    else {
-        currentNumber += inputNumber;
-        document.getElementById("text").innerHTML = currentNumber;
-    }
+    currentNumber += inputNumber;
+    display(currentNumber);
+}
+
+function inputOperation(inputOperation){
+    firstNumber = document.getElementById("text").innerHTML;
+    currentNumber = "";
+    operation = inputOperation;
 }
 
 function clear() {
     currentNumber = "";
-    firstNumber = "";
-    secondNumber = "";
     operation = "";
     document.getElementById("text").innerHTML = "0";
 }
 
-function inputOperation(inputOperation){
-    if (operation != ""){
-        equals();
-        firstNumber = document.getElementById("text").innerHTML
-        operation = inputOperation;
-        currentNumber = "";
-    } else {
-        firstNumber = document.getElementById("text").innerHTML
-        operation = inputOperation
-        currentNumber = "";
-        document.getElementById("text").innerHTML = "0"
+function equals(){
+    switch (operation){
+        case "+":
+            result = Number(firstNumber) + Number(currentNumber);
+            break;
+        case "-":
+            result = Number(firstNumber) - Number(currentNumber);
+            break;
+        case "*":
+            result = Number(firstNumber) * Number(currentNumber);
+            break;
+        case "/":
+            result = Number(firstNumber) / Number(currentNumber);
+            break;
     }
+    display(result);
+    result = "";
+    operation = "";
+    currentNumber = "";
+    firstNumber = "";
 }
 
-function equals(){
-    if (operation != ""){
-        secondNumber = document.getElementById("text").innerHTML
-        switch (operation) {
-            case "+":
-                result = Number(firstNumber) + Number(secondNumber)
-                break;
-            case "-":
-                result = Number(firstNumber) - Number(secondNumber)
-                break;
-            case "*":
-                result = Number(firstNumber) * Number(secondNumber)
-                break;
-            case "/":
-                result = Number(firstNumber) / Number(secondNumber)
-                break;
-        }
-        document.getElementById("text").innerHTML = result
-        operation = "";
-    }
+function display(number){
+    document.getElementById("text").innerHTML = number;
 }
 
 // Button click
-buttonAdd.onclick = function() {inputOperation("+")};
-buttonSubtract.onclick = function() {inputOperation("-")};
-buttonMultiply.onclick = function() {inputOperation("*")};
-buttonDivide.onclick = function() {inputOperation("/")};
-button1.onclick = function() {inputNumber("1")};
-button2.onclick = function() {inputNumber("2")};
-button3.onclick = function() {inputNumber("3")};
-button4.onclick = function() {inputNumber("4")};
-button5.onclick = function() {inputNumber("5")};
-button6.onclick = function() {inputNumber("6")};
-button7.onclick = function() {inputNumber("7")};
-button8.onclick = function() {inputNumber("8")};
-button9.onclick = function() {inputNumber("9")};
-button0.onclick = function() {inputNumber("0")};
-buttonDecimal.onclick = function(){inputNumber(".")};
-buttonSign.onclick = function(){inputNumber("-")};
-buttonClear.onclick = function() {clear()};
-buttonEquals.onclick = function() {equals()};
+buttonAdd.onclick = function() {inputOperation("+")}
+buttonSubtract.onclick = function() {inputOperation("-")}
+buttonMultiply.onclick = function() {inputOperation("*")}
+buttonDivide.onclick = function() {inputOperation("/")}
+button1.onclick = function() {inputNumber("1")}
+button2.onclick = function() {inputNumber("2")}
+button3.onclick = function() {inputNumber("3")}
+button4.onclick = function() {inputNumber("4")}
+button5.onclick = function() {inputNumber("5")}
+button6.onclick = function() {inputNumber("6")}
+button7.onclick = function() {inputNumber("7")}
+button8.onclick = function() {inputNumber("8")}
+button9.onclick = function() {inputNumber("9")}
+button0.onclick = function() {inputNumber("0")}
+buttonDecimal.onclick = function(){inputNumber(".")}
+buttonSign.onclick = function(){inputNumber("-")}
+buttonClear.onclick = function() {clear()}
+buttonEquals.onclick = function() {equals()}
